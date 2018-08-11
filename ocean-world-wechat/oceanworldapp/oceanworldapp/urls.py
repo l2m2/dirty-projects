0ac5eb24ee1ee3f3d24674d4f0a3911b2f5073e2
l2m2lq@gmail.com <114723704@qq.com>
2018-08-12 02:17:17 +0800

    update.

--- ocean-world-wechat/oceanworldapp/oceanworldapp/urls.py
+++ ocean-world-wechat/oceanworldapp/oceanworldapp/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from werobot.contrib.django import make_view
+from robot.robot import robot
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('robot/', make_view(robot)),
 ]
