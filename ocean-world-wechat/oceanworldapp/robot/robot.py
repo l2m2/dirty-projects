3d5ba760c7e7fde3c9c441f1f8958646803fc44a
l2m2lq@gmail.com <114723704@qq.com>
2018-08-12 02:30:09 +0800

    update.

--- ocean-world-wechat/oceanworldapp/robot/robot.py
+++ ocean-world-wechat/oceanworldapp/robot/robot.py
@@ -1,7 +1,17 @@
-import config
 from werobot import WeRoBot
+from werobot.config import Config
 
-robot = WeRoBot(config=config.cfg)
+cfg = Config(
+  SERVER='auto',
+  HOST='0.0.0.0',
+  PORT=80,
+  SESSION_STORAGE=None,
+  TOKEN='dirty2018',
+  APP_ID='wx0328a45eebc76a6d',
+  APP_SECRET='168b9d0da865f46619713dd13ed6ab28'
+)
+
+robot = WeRoBot(config=cfg)
 
 @robot.handler
 def hello(message):
