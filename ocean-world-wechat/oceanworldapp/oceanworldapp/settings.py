6ec8b99b80faa56f14be46e02d26e18b1a0c4912
l2m2lq@gmail.com <114723704@qq.com>
2018-08-11 23:43:33 +0800

    update.

--- ocean-world-wechat/oceanworldapp/oceanworldapp/settings.py
+++ ocean-world-wechat/oceanworldapp/oceanworldapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^1mxg*0ex74xo2k(&%gst5w)srx#w7_+4&kmoh%&50*rcv(3+m'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
