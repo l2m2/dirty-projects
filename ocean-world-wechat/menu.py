0380e18c9a2686c3fe322234593a174a07e1df1d
l2m2lq@gmail.com <114723704@qq.com>
2018-08-11 17:28:30 +0800

    update.

--- ocean-world-wechat/menu.py
+++ ocean-world-wechat/menu.py
@@ -10,42 +10,52 @@ if __name__ == "__main__":
             "入园必看",
             "sub_button": [{
                 "type": "click",
-                "name": "集团官网"
+                "name": "集团官网",
+                "key": "group-official-website"
             }, {
                 "type": "click",
-                "name": "景区介绍"
+                "name": "景区介绍",
+                "key": "introduction"
             }, {
                 "type": "click",
-                "name": "动物科普"
+                "name": "动物科普",
+                "key": "animal-science"
             }, {
                 "type": "click",
-                "name": "表演时间"
+                "name": "表演时间",
+                "key": "show-time"
             }, {
                 "type": "click",
-                "name": "游览攻略"
+                "name": "游览攻略",
+                "key": "tour-guide"
             }]
         }, {
             "name":
             "最新信息",
             "sub_button": [{
                 "type": "click",
-                "name": "主题活动"
+                "name": "主题活动",
+                "key": "activity"
             }, {
                 "type": "click",
-                "name": "招聘信息"
+                "name": "招聘信息",
+                "key": "job-offers"
             }, {
                 "type": "click",
-                "name": "有奖调研"
+                "name": "有奖调研",
+                "key": "award-research"
             }]
         }, {
             "name":
             "购票服务",
             "sub_button": [{
                 "type": "click",
-                "name": "个人中心"
+                "name": "个人中心",
+                "key": "personal-center"
             }, {
                 "type": "click",
-                "name": "一键购票"
+                "name": "一键购票",
+                "key": "one-click-ticket-purchase"
             }]
         }]
     })
