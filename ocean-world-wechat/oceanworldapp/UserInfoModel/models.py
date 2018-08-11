615f37d2b5233738c9a71f6fd19d285f582cc487
l2m2lq@gmail.com <114723704@qq.com>
2018-08-12 01:50:25 +0800

    update.

--- ocean-world-wechat/oceanworldapp/UserInfoModel/models.py
+++ ocean-world-wechat/oceanworldapp/UserInfoModel/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class UserInfo(models.Model):
-  wechatno = models.CharField(max_length=255, unquire=True)
+  wechatno = models.CharField(max_length=255, unique=True)
   fullname = models.CharField(max_length=255, blank=True)
   cellphone = models.CharField(max_length=40)
   sex = models.CharField(max_length=20, blank=True)
