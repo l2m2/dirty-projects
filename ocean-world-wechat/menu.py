b17ef4789b0bf1ad58a370c6ffd0ddb9d2de9043
l2m2lq@gmail.com <114723704@qq.com>
2018-08-11 17:07:36 +0800

    update.

--- ocean-world-wechat/menu.py
+++ ocean-world-wechat/menu.py
@@ -1,23 +1,13 @@
 import werobot
-from werobot.config import Config
+import config
 
 if __name__ == "__main__":
-  config = Config(
-    SERVER='auto',
-    HOST='0.0.0.0',
-    PORT=80,
-    SESSION_STORAGE=None,
-    TOKEN='dirty2018',
-    APP_ID='wx0328a45eebc76a6d',
-    APP_SECRET='168b9d0da865f46619713dd13ed6ab28'
-  )
-
-  robot = werobot.WeRoBot(config=config)
+  robot = werobot.WeRoBot(config=config.cfg)
   client = robot.client
   client.create_menu({
       "button":[{
           "type": "click",
-          "name": "今日歌曲",
+          "name": "测试",
           "key": "music"
       }]
   })
