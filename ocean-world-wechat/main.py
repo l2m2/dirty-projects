5365526a2a9e0cd5a576a76142404216e763c4e8
l2m2lq@gmail.com <114723704@qq.com>
2018-08-11 16:56:13 +0800

    update.

--- ocean-world-wechat/main.py
+++ ocean-world-wechat/main.py
@@ -5,6 +5,7 @@ config = Config(
   SERVER='auto',
   HOST='0.0.0.0',
   PORT=80,
+  SESSION_STORAGE=None,
   TOKEN='dirty2018',
   APP_ID='wx0328a45eebc76a6d',
   APP_SECRET='168b9d0da865f46619713dd13ed6ab28'
