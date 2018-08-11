7fa75b2dd84769c17f70b0f644322b67ab2689f8
l2m2lq@gmail.com <114723704@qq.com>
2018-08-12 01:31:06 +0800

    update.

---
+++ ocean-world-wechat/oceanworldapp/OrderModel/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from OrderModel.models import Order
+
+# Register your models here.
+admin.site.register(Order)
---
+++ ocean-world-wechat/oceanworldapp/OrderModel/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Order(models.Model):
+  username = models.CharField(max_length=255)
+  orderno = models.CharField(max_length=255)
+  content = models.TextField()
+  status = models.CharField(max_length=20)
+  createtime = models.DateTimeField()
