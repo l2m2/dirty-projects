f88b0fe6ab80e504740eedfcd45fd636c88d93a8
l2m2 <l2m2lq@gmail.com>
2018-08-12 17:08:37 +0800

    update.

--- ocean-world-wechat/oceanworldapp/wsgi.py
+++ ocean-world-wechat/oceanworldapp/wsgi.py
@@ -1,4 +1,5 @@
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oceanworldapp.settings")
 application = get_wsgi_application()
