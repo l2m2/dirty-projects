1d34f8be2256ada298e95aa8a70dc940d5acb4ad
l2m2 <l2m2lq@gmail.com>
2018-08-13 01:33:35 +0800

    update.

--- ocean-world-wechat/oceanworldapp/personalcenter/views.py
+++ ocean-world-wechat/oceanworldapp/personalcenter/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.views import View
-from robot.robot import client
 
 class PersonalCenterView(View):
     def get(self, request):
