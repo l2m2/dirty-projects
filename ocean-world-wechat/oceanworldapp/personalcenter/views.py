8a8a7bcad8409dc731129d59c9873c0944683d26
l2m2 <l2m2lq@gmail.com>
2018-08-12 19:35:25 +0800

    update.

--- ocean-world-wechat/oceanworldapp/personalcenter/views.py
+++ ocean-world-wechat/oceanworldapp/personalcenter/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views import View
+from werobot.client import Client
+from robot.robot import cfg 
 
 class PersonalCenterView(View):
     def get(self, request):
-        print("FUCK")
+        Client(config=cfg).grant_token()
